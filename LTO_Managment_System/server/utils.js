0e0fa047c3bbf54b758d34d24d3eab0b97b6e60c
Vihinsa Thejan Bandara <88578813+IT21266300@users.noreply.github.com>
2024-06-06 08:54:13 +0530

    remove some unnecessary parts

--- LTO_Managment_System/server/utils.js
+++ LTO_Managment_System/server/utils.js
@@ -4,15 +4,12 @@ export const generateToken = (member) => {
   return jwt.sign(
     {
       mongoID: member._id,
-      profileImage: member.profileImage,
       name: member.name,
       staffId: member.staffId,
-      username: member.username,
       position: member.position,
       password: member.password,
       phone: member.phone,
-      email: member.email,
-      team: member.team,
+
     },
     process.env.JWT_SECRET,
     {
