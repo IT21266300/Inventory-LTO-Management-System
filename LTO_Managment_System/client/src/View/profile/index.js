4299b04cb78247482aebfe5cf1fbc72056ec2741
Dumindu Lakshan <99247843+IT21266096@users.noreply.github.com>
2024-06-07 16:40:49 +0530

    updated

--- LTO_Managment_System/client/src/View/profile/index.js
+++ LTO_Managment_System/client/src/View/profile/index.js
@@ -51,18 +51,22 @@ const Profile = () => {
             width: '40px',
             height: '40px',
             borderRadius: '100px',
-            backgroundColor: colorPalette.primary[500],
-            color: colorPalette.secondary[100],
+            backgroundColor: colorPalette.yellow[500],
+            color: colorPalette.black[500],
             '&:hover': {
-              backgroundColor: colorPalette.primary[500],
-              color: colorPalette.secondary[100],
+              backgroundColor: colorPalette.yellow[500],
+              color: colorPalette.black[500],
             },
           }}
         >
           <PersonIcon />
         </IconButton>
 
-        <Typography variant="h5" textAlign="center">
+        <Typography
+          variant="h5"
+          textAlign="center"
+          sx={{ color: '#fff', mt: '1rem' }}
+        >
           User Profile
         </Typography>
       </Box>
@@ -71,114 +75,111 @@ const Profile = () => {
           sx={{
             width: '100%',
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
-            alignItems: 'center',
-            flexGrow: 1,
+            alignItems: 'center'
           }}
         >
-          <Grid container spacing={3} sx={{ width: '80%' }}>
-            <Grid item xs={3}>
-              <Box
-                sx={{
-                  width: '100%',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}
-              >
-          
-                <Button
-                  variant="contained"
-                  component="label"
-                  endIcon={<CameraAltIcon />}
-                  sx={{
-                    color: 'white',
-                    padding: '0.6rem 0.9rem',
-                    m: '0.5rem 0', 
-                    width: '200px',
-                    '&:hover': {
-                      backgroundColor: colorPalette.primary[400],
-                      color: colorPalette.secondary[100],
-                    },
-                  }}
-                >
-                  Edit Profile
-                  <input hidden accept="image/*" multiple type="file" />
-                </Button>
-              </Box>
-            </Grid>
-            <Grid item xs={7}>
-              <Stack spacing={2}>
-                <FlexBetween sx={{ mb: '1.5rem' }}>
-                  <TextField
-                    id="outlined-basic"
-                    label="Staff ID"
-                    variant="outlined"
-                    value={userInfo.staffId}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Name"
-                    variant="outlined"
-                    value={userInfo.name}
-                  />
-                </FlexBetween>
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <TextField
-                    id="outlined-basic"
-                    label="Username"
-                    variant="outlined"
-                    value={userInfo.username}
-                    sx={{ mb: '1.5rem' }}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Email"
-                    variant="outlined"
-                    value={userInfo.email}
-                    sx={{ mb: '1.5rem' }}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Phone Number"
-                    variant="outlined"
-                    value={userInfo.phone}
-                    sx={{ mb: '1.5rem' }}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Position"
-                    variant="outlined"
-                    value={userInfo.position}
-                    sx={{ mb: '1.5rem' }}
-                  />
-                  <TextField
-                    id="outlined-basic"
-                    label="Team"
-                    variant="outlined"
-                    value={userInfo.team}
-                    sx={{ mb: '1.5rem' }}
-                  />
-                </Box>
-              </Stack>
-            </Grid>
-            <Grid item xs>
-              <Button
-                variant="contained"
-                onClick={handleUpdate}
-                sx={{
-                  color: 'white',
-                  padding: '0.6rem 0.9rem',
-                  '&:hover': {
-                    backgroundColor: colorPalette.primary[400],
-                    color: colorPalette.secondary[100],
-                  },
-                }}
-              >
-                Update Profile
-              </Button>
-            </Grid>
-          </Grid>
+          <TextField
+            id="outlined-basic"
+            label="Staff ID"
+            variant="outlined"
+            value={userInfo.staffId}
+            sx={{
+              mb: '1.5rem',
+
+              '& .MuiOutlinedInput-root': {
+                color: '#fff',
+                '& .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#ffe404',
+                },
+              },
+              '& .MuiInputLabel-outlined': {
+                color: '#fff',
+              },
+              // "&.Mui-focused": {
+              //   "& .MuiOutlinedInput-notchedOutline": {
+              //     borderColor: "#ffe404",
+              //     borderWidth: "3px",
+              //   },
+              // },
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            value={userInfo.name}
+            sx={{
+              mb: '1.5rem',
+
+              '& .MuiOutlinedInput-root': {
+                color: '#fff',
+                '& .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#ffe404',
+                },
+              },
+              '& .MuiInputLabel-outlined': {
+                color: '#fff',
+              },
+              // "&.Mui-focused": {
+              //   "& .MuiOutlinedInput-notchedOutline": {
+              //     borderColor: "#ffe404",
+              //     borderWidth: "3px",
+              //   },
+              // },
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Phone Number"
+            variant="outlined"
+            value={userInfo.phone}
+            sx={{
+              mb: '1.5rem',
+
+              '& .MuiOutlinedInput-root': {
+                color: '#fff',
+                '& .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#ffe404',
+                },
+              },
+              '& .MuiInputLabel-outlined': {
+                color: '#fff',
+              },
+              // "&.Mui-focused": {
+              //   "& .MuiOutlinedInput-notchedOutline": {
+              //     borderColor: "#ffe404",
+              //     borderWidth: "3px",
+              //   },
+              // },
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Position"
+            variant="outlined"
+            value={userInfo.position}
+            sx={{
+              mb: '1.5rem',
+
+              '& .MuiOutlinedInput-root': {
+                color: '#fff',
+                '& .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#ffe404',
+                },
+              },
+              '& .MuiInputLabel-outlined': {
+                color: '#fff',
+              },
+              // "&.Mui-focused": {
+              //   "& .MuiOutlinedInput-notchedOutline": {
+              //     borderColor: "#ffe404",
+              //     borderWidth: "3px",
+              //   },
+              // },
+            }}
+          />
         </Box>
       </form>
     </Box>
