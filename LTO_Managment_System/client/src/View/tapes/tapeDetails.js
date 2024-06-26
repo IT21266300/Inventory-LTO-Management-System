031805c23a3e08be10b55d416ee9219f4e8b1bcf
Vihinsa Thejan Bandara <88578813+IT21266300@users.noreply.github.com>
2024-06-26 10:20:25 +0530

    tape details add complete

--- LTO_Managment_System/client/src/View/tapes/tapeDetails.js
+++ LTO_Managment_System/client/src/View/tapes/tapeDetails.js
@@ -169,9 +169,10 @@ const ViewTape = () => {
                   color: '#fff',
                 }}
               >
-                <MenuItem value="Completed">Completed</MenuItem>
-                <MenuItem value="In Progress">In Progress</MenuItem>
-                <MenuItem value="Failed">Failed</MenuItem>
+                 <MenuItem value={'Completed'}>Completed</MenuItem>
+                  <MenuItem value={'Failed'}>Failed</MenuItem>
+                  <MenuItem value={'In Progress'}>In Progress</MenuItem>
+                  <MenuItem value={'Not Taken'}>Not Taken</MenuItem>
               </Select>
             </Grid>
 
@@ -447,8 +448,7 @@ const ViewTape = () => {
         <AddNewTapePopup
           open={addNewTapePopupOpen}
           onClose={handleCloseAddNewTapePopup}
-
-          systemId={tapeData ? tapeData.sysId : null} // Pass systemId if available
+          tapeId={tapeData ? tapeData.tapeId : null}// Pass systemId if available
         />
       </Box>
 
