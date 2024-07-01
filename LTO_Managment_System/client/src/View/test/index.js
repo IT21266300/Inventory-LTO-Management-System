e2f6506796cc1d358feee02fb165c40b041b038e
Dumindu Bandara <lakshand969@gmail.com>
2024-07-01 10:25:15 +0530

    updated

--- LTO_Managment_System/client/src/View/test/index.js
+++ LTO_Managment_System/client/src/View/test/index.js
@@ -1,49 +1,5 @@
-import React, { useState } from 'react';
+import React from "react";
 
-// Data for provinces and districts
-const provinceData = {
-  Western: ['Colombo', 'Gampaha', 'Kalutara'],
-  Central: ['Kandy', 'Matale', 'Nuwara Eliya'],
-  // Add other provinces and their districts here
-};
-
-const App = () => {
-  const [selectedProvince, setSelectedProvince] = useState('');
-  const [districts, setDistricts] = useState([]);
-
-  const handleProvinceChange = (e) => {
-    const province = e.target.value;
-    setSelectedProvince(province);
-    setDistricts(provinceData[province] || []);
-  };
-
-  return (
-    <div style={{color: '#fff'}}>
-      <h1>Select Province and District</h1>
-      <div>
-        <label htmlFor="province">Province:</label>
-        <select id="province" value={selectedProvince} onChange={handleProvinceChange}>
-          <option value="">--Select Province--</option>
-          {Object.keys(provinceData).map((province) => (
-            <option key={province} value={province}>
-              {province}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="district">District:</label>
-        <select id="district">
-          <option value="">--Select District--</option>
-          {districts.map((district) => (
-            <option key={district} value={district}>
-              {district}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+export default function index() {
+  return <div>index</div>;
+}
