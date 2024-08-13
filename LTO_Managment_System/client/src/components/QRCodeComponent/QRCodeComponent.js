ddf53b850a8e5538e2e32c412778e81ec2319116
Vihinsa Thejan Bandara <88578813+IT21266300@users.noreply.github.com>
2024-08-13 12:00:12 +0530

    generate QR code

---
+++ LTO_Managment_System/client/src/components/QRCodeComponent/QRCodeComponent.js
@@ -0,0 +1,50 @@
+// components/QRCodeComponent/QRCodeComponent.js
+
+import React from "react";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import QRCode from "qrcode.react";
+
+const QRCodeComponent = ({ tapeId }) => {
+  const handlePrint = () => {
+    window.print();
+  };
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        padding: "2rem",
+        marginBottom: "2rem",
+        borderRadius: "10px",
+        backgroundColor: "#fff",
+        color: "#000",
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        QR Code for Tape ID: {tapeId}
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        <QRCode value={tapeId} size={200} />
+      </Box>
+      <Button
+        variant="contained"
+        onClick={handlePrint}
+        sx={{
+          backgroundColor: "#4CAF50",
+          color: "#fff",
+        }}
+      >
+        Print QR Code
+      </Button>
+    </Paper>
+  );
+};
+
+export default QRCodeComponent;
